42841e0e578e65b9b80dd43b1af80b88c30f6bf7
BoazAkunga <boazakunga@gmail.com>
2023-11-14 13:49:10 -0800

    removed one unistd.h from header file

--- main.h
+++ main.h
@@ -1,10 +1,8 @@
-#ifndef MIAN_H
+#ifndef MAIN_H
 #define MAIN_H
 
-//Functions headers
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <unistd.h>
 
-#endif // MAIN_H
+#endif
