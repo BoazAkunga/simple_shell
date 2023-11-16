c691a9e5ebd1950c3f88abe02707824a4e11a181
BoazAkunga <boazakunga@gmail.com>
2023-11-15 17:59:57 -0800

    made changes in my loop and betty doc style

--- Fwe.c
+++ Fwe.c
@@ -1,8 +1,15 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+
+/**
+ * main - entry point for my program that forks and executes
+ * the "ls -l /tmp" command in 5 child processes.
+ * Return: 0 on success, -1 on failure
+ */
 
 int main(void)
 {
@@ -14,21 +21,22 @@ int main(void)
 	for (b = 1; b <= 5; b++)
 	{
 		C_pid = fork();
-		if(C_pid == -1)
+		if (C_pid == -1)
 		{
 			perror("fork error:");
 			return (-1);
 		}
-	}
 
-	if (C_pid == 0)
-	{
+
+		if (C_pid == 0)
+		{
 		execve("/usr/bin/ls", arg, NULL);
 		perror("fork error:");
 		exit(1);
-	}
-	else
-	{
+		}
+		else
+		{
 		wait(&STATUS);
+		}
 	}
 }
