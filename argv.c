e4d4a31d1c20c5083200eddb21233bc1b405a5d7
donmaleek <bamboloshari@gmail.com>
2023-11-15 17:05:51 -0800

    update

--- argv.c
+++ argv.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
+/*
+ * main - entry point of the function
+ * @argc - resents count
+ * @argv - represents the vector arguments
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	int count;
 
+	for (count = 0; argv[count] != NULL; count++)
+
+		printf("%s\n", *argv);
+
+	while (*argv != NULL)
+		printf("%s\n", *argv), argv++;
+	return (0);
+
+}
